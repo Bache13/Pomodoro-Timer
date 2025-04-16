ac030e47cfe0f455377cd601507d50c22082b028
Bache13 <andrebachman13@gmail.com>
2025-04-16 13:59:54 +0200

    Fixed activated (like a toggle)

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ const btnLongBreak = document.getElementById("btnLongBreak");
 const timerDisplay = document.getElementById("timerDisplay");
 const startButton = document.getElementById("startButton");
 
+function updateActiveButton() {
+
+    btnPomodoro.classList.remove("active");
+    btnShortBreak.classList.remove("active");
+    btnLongBreak.classList.remove("active");
+
+    if (activeMode === "pomodoro") {
+        btnPomodoro.classList.add("active");
+    } else if (activeMode === "short") {
+        btnShortBreak.classList.add("active");
+    } else if (activeMode === "long") {
+        btnLongBreak.classList.add("active");
+    }
+}
+
+
+
