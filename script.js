be848c0cbdf44b484443ab4bfd20323a13ba686c
Bache13 <andrebachman13@gmail.com>
2025-04-16 11:49:20 +0200

    Added base + start to function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+let activeMode = "pomodoro";
+
+const btnPomodoro = document.getElementById("btnPomodoro");
+const btnShortBreak = document.getElementById("btnShortBreak");
+const btnLongBreak = document.getElementById("btnLongBreak");
+const timerDisplay = document.getElementById("timerDisplay");
+const startButton = document.getElementById("startButton");
+
